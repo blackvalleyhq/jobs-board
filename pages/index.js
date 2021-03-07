e0aa043266650551d8457035f2753d5ef0db73ee
Joe Reed <joe.reed@moneyhub.com>
2021-03-07 19:54:11 +0000

    Define rough text styles

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,10 @@ export default function Home({ allJobs }) {
           href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
