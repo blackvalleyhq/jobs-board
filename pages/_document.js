46c6ff397ca1ed192f72cec08d6452f40bc9d4d2
Hadiyah <62774630+HadiyahL@users.noreply.github.com>
2021-03-26 22:41:08 +0000

    updated favicon

--- pages/_document.js
+++ pages/_document.js
@@ -41,7 +41,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/bv-caret.svg" />
         </Head>
         <body>
           <Main />
